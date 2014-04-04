c65923b284e22b12ec1129b1a960e75a0c2d1e30
Alexander Vershilov <alexander.vershilov@gmail.com>
2014-04-04 18:20:06 +0400

    fix access violation error

--- qnikst.c
+++ qnikst.c
@@ -5,11 +5,12 @@
 char * undebugq(char *in, char *needle) {
     char *p = in;
     size_t c = 0, l = strlen(in), nl = strlen(needle);
-    for (p = in; *p; p++, c++) {
+
+    for (; *p; c++) {
         for (; *(p) == ' '; (p)++);
         if (*p == *needle) {
             if (l < c + nl) { return in; }
-            if (strncmp(p, needle, nl) == 0) {
+            if (! strncmp(p, needle, nl)) {
                 if (p[nl] == ' ') {
                   memmove(p, p + nl + 1, l - nl + 1); // we need to copy \0
                   l -= nl;
@@ -24,21 +25,3 @@ char * undebugq(char *in, char *needle) {
     }
     return in;
 }
-/*
-main() {
-   static char *tests[] = {
-      "debug","debugfs","debug=1","debug systemd.debug","debug 123 debug 456 debug","debu",NULL
-      };
-   char r[]="debug",*s;
-   int t;
-   for(t=0;(s=tests[t])!=NULL;t++) {
-     int len;
-     s = strdup(s);  // make writable copy
-     printf("string=\"%s\"\n",s);
-     undebug(s);
-     printf("result=\"%s\"\n\n",s);
-     free(s);
-   }
-}
-*/
-
